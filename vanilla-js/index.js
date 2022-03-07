23c8926258de49716e14bd64795b92aaa6eeeb7c
nssys <86tkstar@gmail.com>
2022-03-07 10:57:49 +0900

    #

---
+++ vanilla-js/index.js
@@ -0,0 +1,10 @@
+const button = document.querySelector("button");
+button.addEventListener("click", showModal);
+
+// const showModal = () => {
+//     console.log("click");
+// }
+
+function showModal () {
+    console.log("click");
+}
